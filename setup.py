6737d5511f262d47265dfbbd4211356afbe2ebaa
Derek J. Curtis <derek.curtis@apps-foundry.com>
2017-08-21 18:58:52 +0700

    Added setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='GramediaCommon',
+    version='0.1.0',
+    packages=[
+        'gramedia',
+        'gramedia.common'
+    ],
+    url='https://engineering.gramedia.com',
+    license='',
+    author='Backend Team',
+    author_email='backend@staff.gramedia.com',
+    description='Basic python 3.6 helper classes and functions'
+)
