e3c05e4aff63fb9980d4d966c86cee0caad69549
Heri Priyatno <heri.priyatno@gramedia.digital>
2023-06-06 15:12:51 +0700

    fix: 0.9.2 versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, PT Gramedia Digital Nusantara'
 author = 'PT Gramedia Digital Nusantara'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.9.2'
 
 
 # -- General configuration ---------------------------------------------------
